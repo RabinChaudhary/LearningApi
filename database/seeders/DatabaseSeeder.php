96b77933d094fe7736449e396130e1f9af91e72f
Rabin Chaudhary <rabinch1011@gmail.com>
2022-06-20 12:21:33 +1000

    basic rest api learned

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Petition;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,5 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Petition::factory(50)->create();
+        
     }
 }
